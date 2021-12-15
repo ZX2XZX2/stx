b56a36376564033e8915783e021c1fcc0e715b26
ZX2XZX2 <zx2xzx27979@gmail.com>
2021-12-15 11:44:13 -0500

    Shell that triggers python datafeed

---
+++ shell/datafeed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                                     
+export CRT_DIR=${PWD}
+BASE_DIR=${HOME}/stx
+source ${HOME}/.env_vars
+source ${HOME}/.envs/stx/bin/activate
+cd ${BASE_DIR}/python
+export LANG=en_US.UTF-8
+python3 stxdf.py
+cd ${CRT_DIR}
