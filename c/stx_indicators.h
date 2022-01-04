be6205bf8357fe9aaf2dbd441c9e7c14c0834061
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-01-03 18:22:29 -0800

    Added functions that calculate relative strength

--- c/stx_indicators.h
+++ c/stx_indicators.h
@@ -11,8 +11,8 @@
 #include "stx_ts.h"
 #include <time.h>
 
-#define INCREASING_ORDER 0
-#define DECREASING_ORDER 1
+#define ASCENDING_ORDER 0
+#define DESCENDING_ORDER 1
 /** Code to sort the stocks.  Initially used to calculate relative
  *  strength, IBD style.
  */
@@ -35,9 +35,9 @@ void indicators_shell_sort(eq_value_ptr recs, int num, int order) {
     for(ixxx = num / 2; ixxx > 0; ixxx /= 2) {
         for(ixx = ixxx; ixx < num; ixx++) {
             for(ix = ixx - ixxx; ix >= 0; ix -= ixxx) {
-                if ((order == INCREASING_ORDER &&
+                if ((order == ASCENDING_ORDER &&
                      recs[ix].value < recs[ix + ixxx].value) ||
-                    (order == DECREASING_ORDER &&
+                    (order == DESCENDING_ORDER &&
                      recs[ix].value > recs[ix + ixxx].value)) {
                     memset(&temp, 0, sizeof(eq_value));
                     strcpy(temp.ticker, recs[ix].ticker);
@@ -89,5 +89,75 @@ void indicators_rank(eq_value_ptr records, char* dt, char* indicator_name,
         processed += current_bucket_size;
         printf(")\n");
     }
+}
+
+int stock_relative_strength(stx_data_ptr data, int rs_days) {
+    int rsd1 = rs_days / 4, rsd2 = rs_days / 2, ix = data->pos;
+    float rs_1, rs_2, rs_3, res;
+    float cc = (float)data->data[ix].close;
+    float cc_0 = (float)data->data[0].close;
+    float cc_1 = (rsd1 > 1)? (float)data->data[ix + 1 - rsd1].close:
+        (float) data->data[ix].open;
+    float cc_2 = (rsd2 > 1)? (float) data->data[ix + 1 - rsd2].close:
+        (float) data->data[ix].open;
+    float cc_3 = (rs_days > 1)? (float)data->data[ix + 1 - rs_days].close:
+        (float) data->data[ix].open;
+    if (ix >= rs_days - 1) {
+        rs_1 = (cc_1 == 0)? 0: cc / cc_1 - 1;
+        rs_2 = (cc_2 == 0)? 0: cc / cc_2 - 1;
+        rs_3 = (cc_3 == 0)? 0: cc / cc_3 - 1;
+    } else if (ix >= rsd2 - 1) {
+        rs_1 = (cc_1 == 0)? 0: cc / cc_1 - 1;
+        rs_2 = (cc_2 == 0)? 0: cc / cc_2 - 1;
+        rs_3 = (cc_0 == 0)? 0: cc / cc_0 - 1;
+    } else if (ix >= rsd1 - 1) {
+        rs_1 = (cc_1 == 0)? 0: cc / cc_1 - 1;
+        rs_2 = (cc_0 == 0)? 0: cc / cc_0 - 1;
+        rs_3 = rs_2;
+    } else {
+        rs_1 = (cc_0 == 0)? 0: cc / cc_0 - 1;
+        rs_2 = rs_1;
+        rs_3 = rs_1;
+    }
+    res = 40 * rs_1 + 30 * rs_2 + 30 * rs_3;
+    return (int) res;
+}
+
+void indicators_stock_relative_strength(cJSON *tickers, char* asof_date,
+                                        int num_days) {
+    cJSON *ticker = NULL;
+    int num = 0, total = cJSON_GetArraySize(tickers);
+    char rs_name[8];
+    memset(rs_name, 0, 8);
+    sprintf(rs_name, "RS_%d", num_days);
+    LOGINFO("%s for %d tickers as of %s\n", rs_name, total, asof_date);
+    eq_value_ptr rs = (eq_value_ptr) calloc((size_t)total, sizeof(eq_value));
+    memset(rs, 0, total * sizeof(eq_value));
+    cJSON_ArrayForEach(ticker, tickers) {
+        if (cJSON_IsString(ticker) && (ticker->valuestring != NULL)) {
+            eq_value_ptr crt_rs = rs + num;
+            strcpy(crt_rs->ticker, ticker->valuestring);
+            stx_data_ptr data = ts_get_ts(crt_rs->ticker, asof_date, 0);
+            if (data == NULL) {
+                LOGERROR("No data for %s. Wont calc RS\n", crt_rs->ticker);
+                crt_rs->value = 0;
+            } else
+                crt_rs->value = stock_relative_strength(data, num_days);
+        }
+        num++;
+        if (num % 100 == 0)
+            LOGINFO("%s for %4d / %4d tickers as of %s\n", rs_name, num, total,
+                    asof_date);
+    }
+    LOGINFO("%s for %4d / %4d tickers as of %s\n", rs_name, num, total,
+            asof_date);
+    indicators_rank(rs, asof_date, rs_name, total, 100, DESCENDING_ORDER);
+    LOGINFO("%s: sorted tickers in RS descending order\n", rs_name);
+    LOGINFO("%s: ranked tickers and inserted in DB\n", rs_name);
+    if (rs != NULL) {
+        free(rs);
+        rs = NULL;
+    }
+    LOGINFO("%s: freed the data\n", rs_name);
 }
 #endif
