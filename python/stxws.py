ba4782b4c3e66475fa01e160f219afa7bd67cf34
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-06-22 04:04:32 -0400

    Add simple function that plots the three indexes

--- python/stxws.py
+++ python/stxws.py
@@ -1,10 +1,22 @@
 from flask import Flask
 import os
-
+import stxcal
+from stxplot import StxPlot
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'stxws'
+    end_date = stxcal.current_busdate()
+    start_date = stxcal.move_busdays(end_date, -90)
+    res = []
+    res.append('<html>')
+    res.append(f'<title>Indexes {end_date}</title>')
+    res.append('<body>')
+    for stxindex in ['^GSPC', '^IXIC', '^DJI']:
+        sp = StxPlot(None, stxindex, start_date, end_date, stk=stxindex)
+        res.append(sp.webchartstream())
+    res.append('</body>')
+    res.append('</html>')
+    return ''.join(res)
