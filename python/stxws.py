eae4fa6daefff9f67a7c9dc362283952b3b0d4ae
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-06-30 01:22:19 -0400

    WIP: plot intraday charts

--- python/stxws.py
+++ python/stxws.py
@@ -4,6 +4,7 @@ matplotlib.use('Agg')
 import os
 import stxcal
 from stxplot import StxPlot
+from stxplotid import StxPlotID
 
 app = Flask(__name__)
 
@@ -57,6 +58,30 @@ def charts():
                 chartdict = { 'figdata_png': sp.b64_png() }
                 charts.append(chartdict)
     return render_template('charts.html', charts=charts, stx=stks, dt=dt)
+
+
+@app.route('/idcharts', methods=('GET', 'POST'))
+def idcharts():
+    charts = []
+    stks = ''
+    end_dt = f'{stxcal.current_busdate(hr=10)} 16:00'
+    if request.method == 'POST':
+        stks = request.form['stocks']
+        end_dt = request.form['datetime']
+        if not stks:
+            flash('Stocks are required!')
+        elif not end_dt:
+            flash('Date is required!')
+        else:
+            stk_list = stks.split(' ')
+            end_date, end_time = end_dt.split(' ')
+            start_date = stxcal.move_busdays(end_date, -10)
+            start_dt = f'{start_date} 09:35'
+            for stk in stk_list:
+                sp = StxPlotID(stk, start_dt, end_dt, 15)
+                chartdict = { 'figdata_png': sp.b64_png() }
+                charts.append(chartdict)
+    return render_template('charts_intraday.html', charts=charts, stx=stks, dt=end_dt)
 
 
 @app.route('/scanners')
