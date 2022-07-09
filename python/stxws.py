cff974465381cce6f9f9aaa03757390f461c7302
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-07-09 00:07:23 -0400

    Add navigation bar to index page

--- python/stxws.py
+++ python/stxws.py
@@ -24,18 +24,14 @@ stx_ana = StxAnalyzer(indicator_names, indicator_tenors, display_days)
 
 @app.route('/')
 def index():
+    charts = []
     end_date = stxcal.current_busdate(hr=10)
     start_date = stxcal.move_busdays(end_date, -90)
-    res = []
-    res.append('<html>')
-    res.append(f'<title>Indexes {end_date}</title>')
-    res.append('<body>')
     for stxindex in ['^GSPC', '^IXIC', '^DJI']:
         sp = StxPlot(None, stxindex, start_date, end_date, stk=stxindex)
-        res.append(sp.webchartstream())
-    res.append('</body>')
-    res.append('</html>')
-    return ''.join(res)
+        chartdict = { 'figdata_png': sp.b64_png() }
+        charts.append(chartdict)
+    return render_template('indexes.html', charts=charts)
 
 
 @app.route('/indexes')
@@ -86,6 +82,7 @@ def idcharts():
         elif not end_dt:
             flash('Date is required!')
         else:
+            
             stk_list = stks.split(' ')
             end_date, end_time = end_dt.split(' ')
             start_date = stxcal.move_busdays(end_date, -10)
