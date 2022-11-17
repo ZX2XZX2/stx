98ac4493b74b28b75b31d7345aeffecd221092b5
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-11-17 03:22:25 -0500

    WIP: add function to move intraday one 5-min ticker

--- c/stx_core.h
+++ c/stx_core.h
@@ -838,8 +838,35 @@ unsigned long cal_tsfromdt(char *dt) {
  *  Move 5 minutes ahead (if dir is 1) or behind (dir is -1) in time.
  */
 char *cal_move_5mins(char *dt, int dir) {
-    char *res = NULL;
-    return res;
+    char *res = NULL, tmp_dt[20];
+    struct tm *ts;
+    memset(ts, 0, sizeof(struct tm));
+    memset(tmp_dt, 0, 20 * sizeof(char));
+    strcpy(tmp_dt, dt);
+    if (strptime(dt, "%Y-%m-%d %H:%M:%S", ts) == NULL) {
+        LOGERROR("strptime failed for datetime %s\n", dt);
+        return NULL;
+    }
+    time_t tt = mktime(ts);
+    if (dir == 1 && ts->tm_hour == 15 && ts->tm_min == 55) {
+        char *hhmm = strchr(tmp_dt, ' ');
+        *hhmm = '\0';
+        char *next_dt = NULL;
+        cal_next_bday(cal_ix(tmp_dt), &next_dt);
+        sprintf(dt, "%s 09:30:00", next_dt);
+    } else if (dir == -1 && ts->tm_hour == 9 && ts->tm_min == 30) {
+        char *hhmm = strchr(tmp_dt, ' ');
+        *hhmm = '\0';
+        char *prev_dt = NULL;
+        cal_prev_bday(cal_ix(tmp_dt), &prev_dt);
+        sprintf(dt, "%s 15:55:00", prev_dt);
+    }
+    else {
+        tt += (dir * 300);
+        ts = localtime(&tt);
+        strftime(dt, strlen(dt), "%Y-%m-%d %H:%M:%S", ts);
+    }
+    return dt;
 }
 
 /** These two hashtables keep in memory data or JL records calculated
