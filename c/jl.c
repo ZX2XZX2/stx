3bcbf04782b5bd1030fa2930cca4807481416b7e
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-12-01 00:08:42 -0500

    Added intraday parameter

--- c/jl.c
+++ c/jl.c
@@ -8,12 +8,12 @@
 #include "stx_ts.h"
 
 int main(int argc, char** argv) {
-    char stk[16], ed[16];
+    char stk[16], ed[20];
     strcpy(stk, "NFLX");
-    strcpy(ed, cal_current_busdate(0));
+    strcpy(ed, cal_current_trading_datetime());
     float factor = 1.5;
     int num_pivots = 0, num_lines = 3;
-    bool print_pivots_only = false, print_nils = false;
+    bool print_pivots_only = false, print_nils = false, intraday = false;
 
     for (int ix = 1; ix < argc; ix++) {
         if (!strcmp(argv[ix], "-s") && (++ix < argc))
@@ -26,17 +26,29 @@ int main(int argc, char** argv) {
             num_pivots = atoi(argv[ix]);
         else if (!strcmp(argv[ix], "-l") && (++ix < argc))
             num_lines = atoi(argv[ix]);
+        else if (!strcmp(argv[ix], "-i") || !strcmp(argv[ix], "--intraday") )
+            intraday = true;
         else if (!strcmp(argv[ix], "-ppo"))
             print_pivots_only = true;
         else if (!strcmp(argv[ix], "-pnils"))
             print_nils = true;
     }
-    stx_data_ptr data = ts_load_stk(stk, NULL, 0, false);
+    if (intraday) {
+        if (strlen(ed) == 10)
+            strcat(ed, " 15:55:00");
+    } else {
+        char *hhmm = strchr(ed, ' ');
+        if (hhmm != NULL)
+            *hhmm = '\0';
+    }
+    LOGINFO("ed = %s\n", ed);
+    stx_data_ptr data = ts_load_stk(stk, NULL, 0, intraday);
+    LOGINFO("Loaded %d records for %s\n", data->num_recs, data->stk);
     jl_data_ptr jl = jl_jl(data, ed, factor);
     jl_print(jl, print_pivots_only, print_nils);
     for(int ix = jl->pos - num_lines; ix <= jl->pos; ix++) {
         ts_print_record(&(jl->data->data[ix]));
-        fprintf(stderr, "\n");
+        /* fprintf(stderr, "\n"); */
     }
     int num, ixx;
     jl_print_pivots(jl, num_pivots, &num);
