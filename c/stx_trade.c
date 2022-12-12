32f90ad8905418f25dbc28d18e8dfa5f6cba2c5a
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-12-12 02:24:53 -0500

    WIP: added doc and init requirements

--- c/stx_trade.c
+++ c/stx_trade.c
@@ -14,14 +14,23 @@ void sig_handler(int dummy) {
     keep_running = false;
 }
 
-int main(int argc, char** argv) {
 
+/**
+ *  This is supposed to run in two modes: realtime and simulation.
+ *  Realtime runs during trading hours, get the data and analyzes it.
+ *  Cannot control the speed; 'sleep_interval' can be used however, to
+ *  specify how often to refresh the data and analyze it intraday.
+ *  Simulation runs using historical data.  When in simulation mode,
+ *  can control how fast it runs, can eliminate sleep altogether and
+ *  replace it with command to move from input; also we can skip
+ *  certain times if no events happened.
+ */
+int main(int argc, char** argv) {
     char mkt_name[32], dir_name[128], *start_date = NULL;
     int sleep_interval = 300;
     bool realtime = false;
     memset(mkt_name, 0, 32);
     memset(dir_name, 0, 128);
-
     for (int ix = 1; ix < argc; ix++) {
         if ((!strcmp(argv[ix], "-m") || !strcmp(argv[ix], "--market-name")) &&
              (ix++ < argc - 1))
@@ -37,6 +46,14 @@ int main(int argc, char** argv) {
             start_date = cal_move_to_bday(argv[ix], true);
     }
     signal(SIGINT, sig_handler);
+    /**
+     *  If running in realtime, need to check, during initialization,
+     *  that we are prepared to run intraday analysis - the stock list
+     *  is defined, all the data is stored in cache, etc.
+     *
+     *  Same thing for simulation, but two cases, either resuming a
+     *  previous simulation, or starting a new one.
+     */
     while (keep_running) {
         fprintf(stderr, "Still running ...\n");
         sleep(sleep_interval);
