2705345748b56ca0c72c0a7c42932765d1541648
ZX2XZX2 <zx2xzx27979@gmail.com>
2022-12-14 00:13:36 -0500

    Skeleton for file save and load market functions

---
+++ c/stx_trade.h
@@ -0,0 +1,16 @@
+#ifndef __STX_TRADE_H__
+#define __STX_TRADE_H__
+
+#include <cjson/cJSON.h>
+#include "stx_ana.h"
+#include "stx_core.h"
+
+void load_market() {
+
+}
+
+void save_market() {
+
+}
+
+#endif
