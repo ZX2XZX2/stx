d806e3bba1fec2feb22736100efda0635744f9f6
ZX2XZX2 <zx2xzx27979@gmail.com>
2023-02-06 00:36:30 -0500

    Install C library in /usr/local/bin

--- c/libcompile.sh
+++ c/libcompile.sh
@@ -2,7 +2,7 @@
 
 export SRC_FILE=$1
 export LIB_FILE="${SRC_FILE/\.c/.so}"
-export TGT_FILE="/usr/local/sbin/${LIB_FILE}"
+export TGT_FILE="/usr/local/bin/${LIB_FILE}"
 echo -e "gcc -fPIC -shared -o ${LIB_FILE} ${SRC_FILE} -O3 -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl"
 gcc -fPIC -shared -o ${LIB_FILE} ${SRC_FILE} -O3 -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl
 echo -e "sudo cp ${LIB_FILE} ${TGT_FILE}"
