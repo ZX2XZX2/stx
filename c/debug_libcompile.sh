2f142ca696a3bcb2e36d848de45ba97ec9a0bfa7
ZX2XZX2 <zx2xzx27979@gmail.com>
2023-02-07 00:55:24 -0500

    Script to compile debug version of the library

---
+++ c/debug_libcompile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export SRC_FILE=$1
+export LIB_FILE="${SRC_FILE/\.c/.so}"
+export TGT_FILE="/usr/local/bin/${LIB_FILE}"
+echo -e "gcc -fPIC -shared -o ${LIB_FILE} ${SRC_FILE} -g -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl"
+gcc -fPIC -shared -o ${LIB_FILE} ${SRC_FILE} -g -I. -I/usr/include/postgresql -lpq -lm -lcjson -lcurl
+echo -e "sudo cp ${LIB_FILE} ${TGT_FILE}"
+sudo cp ${LIB_FILE} ${TGT_FILE}
