3587e4dc832e76cf5826f244dc0cc48779e9aa70
ZX2XZX2 <zx2xzx27979@gmail.com>
2023-02-11 03:03:12 -0500

    Add header file for stx_lib library

---
+++ c/stx_lib.h
@@ -0,0 +1,29 @@
+#ifndef __STX_LIB_H__
+#define __STX_LIB_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include "stx_core.h"
+#include "stx_ts.h"
+
+#define NUM_EOD_DAYS 300
+#define NUM_ID_DAYS 20
+
+/**
+ *  This is candlev = OHLC candle + volume.  Dates are sent separately
+ *  to python, as the index of the dataframe.
+ */
+typedef struct candlev_t {
+    int open;
+    int high;
+    int low;
+    int close;
+    int volume;
+} candlev, *candlev_ptr;
+
+stx_data_ptr stx_load_stk(char *stk, char *dt, int num_days, bool intraday);
+
+void stx_get_ohlcv(char *stk, char *dt, int num_days, bool intraday,
+                   bool realtime, char** dates, candlev_ptr *candlevs,
+                   int *num_recs);
+#endif
